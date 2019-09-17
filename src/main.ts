49f0a6430538407f5e30f188290df11d9998d533
Timot <timortartakovsky@gmail.com>
2019-09-17 12:50:49 +0300

    prod

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
-// import { config } from 'dotenv';
-// import { displayEnv } from 'dotenv-display';
-// import * as path from 'path';
 
 async function bootstrap() {
   const logger = new Logger('Root');
-  // loadConfig();
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
-  const port  = (serverConfig && serverConfig.port) || process.env.PORT || 8008;
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+  }
+
+  const port = process.env.PORT || (serverConfig && serverConfig.port);
   await app.listen(port, () => {
     logger.log(`Server started on port: ${ port }`);
   });
 }
-
-// const loadConfig = (): void => {
-//   const configPath = path.join(__dirname, '../.env');
-//   const env = config({ path: configPath });
-//   displayEnv(env.parsed);
-// };
 
 bootstrap();
