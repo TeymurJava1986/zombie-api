a89599de6f04f634e72354c73fe20c41dda0dc11
Timot <timortartakovsky@gmail.com>
2019-09-17 10:44:40 +0300

    prod version

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,17 +3,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 const SOURCE_PATH = process.env.NODE_ENV === 'production' ? 'dist' : 'src';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'mongodb',
+    type: 'postgres',
     host: process.env.DB_HOST,
-    port: 27017,
+    port: 5432,
     database: process.env.DB_NAME,
     url: process.env.DB_URL,
     entities: [`${__dirname}/../**/*.entity.{js,ts}`],
     synchronize: true,
-    useNewUrlParser: true,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    useUnifiedTopology: true,
+    username: 'teymurjava',
+    password: 'Pa$$w0rd_1986',
     ssl: process.env.DB_USE_SSL === 'true',
 };
 
